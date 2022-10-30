b36bc9b94223cba0a4ccd4c78252969f26543a8d
Reagan-C <onwuegbureagangmail.com>
2022-10-30 20:52:52 +0100

    first commit

---
+++ src/test/java/com/reagan/webservertask/WebMainTests.java
@@ -0,0 +1,13 @@
+package com.reagan.webservertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
